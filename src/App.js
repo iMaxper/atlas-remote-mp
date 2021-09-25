132e0c5f558e0c6f8cc04840aa2f776163ff7c2e
Maxwell <jpereira4@gmail.com>
2021-09-25 19:13:38 +0000

    island info added into cards

--- src/App.js
+++ src/App.js
@@ -4,19 +4,23 @@ import Card from './components/Card.js';
 import data from './atlas-of-remote-islands.js';
 
 function App() {
-  console.log(data);
-  
+
+let island1 = data.islands[0]
+
+
   return (
     <div>
       <div className="container">
-        <h1 className="section-title">Atlas of Remote Islands</h1>
+        <h1 className="section-title">Atlas of Remote Islands (updated 092521)</h1>
       </div>
       
       <div className="container">
         <h2 className="section-title">Islands</h2>
         <ul className="cards">
           
-          <Card />
+        {data.islands.map(island => 
+        <Card name = {island.name} description = {island.description} latitude = {island.coords.lat} longitude = {island.coords.lon}/>
+      )}
           
         </ul>
       </div>
